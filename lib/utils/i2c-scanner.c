19f88175c76bcff016dac1dc0a5ae09402c04b46
Lemonochrme <marin-muller@insa-toulouse.fr>
2024-12-04 22:37:36 +0100

    Fixed i2c-scanner

--- lib/utils/i2c-scanner.c
+++ lib/utils/i2c-scanner.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
-#include "esp_log.h"
-#include "driver/i2c.h"
 #include "i2c-scanner.h"
+#include "esp_log.h"
 
-void init_i2c() {
+void i2c_scanner_init(i2c_port_t i2c_num, gpio_num_t sda_io, gpio_num_t scl_io, uint32_t clk_speed) {
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = 8,
-        .scl_io_num = 9,
+        .sda_io_num = sda_io,
+        .scl_io_num = scl_io,
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = 100000,
+        .master.clk_speed = clk_speed,
     };
-    i2c_param_config(I2C_NUM_0, &conf);
-    ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
+    ESP_ERROR_CHECK(i2c_param_config(i2c_num, &conf));
+    ESP_ERROR_CHECK(i2c_driver_install(i2c_num, I2C_MODE_MASTER, 0, 0, 0));
 }
 
-void scan_i2c() {
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+void i2c_scan_devices(i2c_port_t i2c_num) {
+    for (int i = 0; i < 128; i++) {
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_stop(cmd);
 
-        for (int i = 0; i < 128; i++) {
-            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1);
-            i2c_master_stop(cmd);
-            esp_err_t ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
-            i2c_cmd_link_delete(cmd);
+        esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 10 / portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
 
-            if (ret == ESP_OK) {
-                printf("I2C device found at address 0x%02x\n", i);
-            }
+        if (ret == ESP_OK) {
+            printf("Found device at address 0x%02x\n", i);
         }
     }
 }
--- lib/utils/i2c-scanner.h
+++ lib/utils/i2c-scanner.h
@@ -1,7 +1,9 @@
 #ifndef I2C_SCANNER_H
 #define I2C_SCANNER_H
 
-void init_i2c();
-void scan_i2c();
+#include "driver/i2c.h"
+
+void i2c_scanner_init(i2c_port_t i2c_num, gpio_num_t sda_io, gpio_num_t scl_io, uint32_t clk_speed);
+void i2c_scan_devices(i2c_port_t i2c_num);
 
 #endif // I2C_SCANNER_H
